8ad6eead52ba064d7ae85847514872368ceb2527
Max R <2contact.mjr@gmail.com>
2019-07-18 23:57:40 -0400

    removed obsolete files

--- Websites/My-First-Website/create-ec2.sh
+++ Websites/My-First-Website/create-ec2.sh
@@ -1,4 +1,6 @@
+docker ps -q | xargs -i docker kill {}
 docker images max-html-image -q | xargs -i docker rmi {}
 docker build -t max-html-image:v1 .
 docker save --output max-html-image.tar max-html-image:v1
+docker run -d -p 8080:80 max-html-image:v1
 #aws s3 cp ./max-html-image.tar s3://123-567-my-test-bucket-us-east-2
